1cc7ee67166382df92df06d70bbf17d3b6b3512a
Buth <sereybuth120@gmail.com>
2023-07-23 19:59:49 +0700

    add Auth DTO

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,14 @@
-import { Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { Request } from 'express';
+import { AuthDto } from './dto';
 
 @Controller('/auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('/signup')
-  signup(@Req() req: Request) {
-    console.log(req.body);
-    return this.authService.signUp();
+  signup(@Body() body: AuthDto) {
+    return this.authService.signUp(body);
   }
 
   @Post('/login')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable({})
 export class AuthService {
@@ -8,7 +9,7 @@ export class AuthService {
     return 'login';
   }
 
-  signUp() {
-    return 'signUp';
+  signUp(body: AuthDto) {
+    return body;
   }
 }
