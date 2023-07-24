f8f1553e8deb28555015f6287e99c46b298fadc4
Buth <sereybuth120@gmail.com>
2023-07-24 22:05:09 +0700

    add guard to the app

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 
@@ -11,6 +11,7 @@ export class AuthController {
     return this.authService.signUp(body);
   }
 
+  @HttpCode(HttpStatus.OK) // return 200.
   @Post('/login')
   login(@Body() body: AuthDto) {
     return this.authService.login(body);
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
 
+@UseGuards(JwtGuard) // make everything require a token
 @Controller('user')
 export class UserController {
   @Get('/me')
-  getMe() {
-    return 'user info';
+  getMe(@GetUser() user: User) {
+    return user;
   }
 }
